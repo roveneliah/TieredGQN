broadcast cam input in repnet, conv2d and u dimensions fixed on gen. net

GQN/encoder.py
@@ -1,70 +1,87 @@
 import keras
 import numpy as np
 import tensorflow as tf
+from keras import backend as K
+from data_reader import DataReader
+
 """
 Implementation of tower network from
 "Neural scene representation and rendering"
 """
-# input 1: 64x64x3
-input_vis = keras.layers.Input(shape=(64,64,3), name="vis")
+def Encoder():
+    # input 1: 64x64x3
+    input_vis = keras.layers.Input(shape=(64,64,3), name="context")
 
-# conv1 kernel 2x2, stride 2x2
-# ==> 32x32x256
-# W = (Wo - kernel_w + 2padding)/stride_w + 1
-# H = (Ho - kernel_h + 2padding)/stride_h + 1
-# D = number of filters
-conv1 = keras.layers.Conv2D(
-            filters=256,
-            kernel_size=(2,2),
-            strides=(2, 2),
-            padding='valid',
-            activation='relu'
-        )(input_vis)
+    # conv1 kernel 2x2, stride 2x2
+    # ==> 32x32x256
+    # W = (Wo - kernel_w + 2padding)/stride_w + 1
+    # H = (Ho - kernel_h + 2padding)/stride_h + 1
+    # D = number of filters
+    conv1 = keras.layers.Conv2D(
+                filters=256,
+                kernel_size=(2,2),
+                strides=(2, 2),
+                padding='valid',
+                activation='relu'
+            )(input_vis)
 
 
-# conv2 kernel 3x3, stride 1x1
-#  ===> 32x32x128
-# 128 filters, but shouldn't the W and H be 30?  Why isn't padding stated??  Should I just assume padding?
-skip_conv2 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), padding='same')(conv1)
-conv2 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), activation='relu', padding='same')(conv1)
+    # conv2 kernel 3x3, stride 1x1
+    #  ===> 32x32x128
+    # 128 filters, but shouldn't the W and H be 30?  Why isn't padding stated??  Should I just assume padding?
+    skip_conv2 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), padding='same')(conv1)
+    conv2 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), activation='relu', padding='same')(conv1)
 
-# Residual connection between conv1 and conv1
-# TODO: is this right? other implementations have sizing issues...
-conv2r = keras.layers.add([skip_conv2, conv2])
+    # Residual connection between conv1 and conv1
+    conv2r = keras.layers.add([skip_conv2, conv2])
 
-# conv3 kernel 2x2, stride 2x2
-#  ====> 16x16x256 (256 filters)
-conv3 = keras.layers.Conv2D(filters = 256, kernel_size = (2,2), strides = (2,2), activation='relu', padding='same')(conv2)
+    # conv3 kernel 2x2, stride 2x2
+    #  ====> 16x16x256 (256 filters)
+    conv3 = keras.layers.Conv2D(filters = 256, kernel_size = (2,2), strides = (2,2), activation='relu', padding='same')(conv2)
 
-# input 2: 1x1x7
-input_pos = keras.layers.Input(shape=(16,16,7), name="pos")
-# print(input_pos)
-# input_pos = tf.broadcast_to(input_pos, [16,16,7])
-# TODO: reshape input to 16x16x7
+    # input 2: 1x1x7
+    input_pos = keras.layers.Input(shape=(1,1,7), name="camera")
+    # reshape input to 16x16x7
+    b1 = keras.layers.Lambda(lambda x: K.repeat_elements(x, 16, 1))(input_pos)
+    b2 = keras.layers.Lambda(lambda x: K.repeat_elements(x, 16, 2))(b1)
 
-# concatenate input_pos onto conv3
-conv3a = keras.layers.concatenate([conv3, input_pos])
+    # concatenate b2 onto conv3
+    conv3a = keras.layers.concatenate([conv3, b2])
 
-# conv4 kernel 3x3, stride 1x1
-# ---> 16x16x128filters
-skip_conv4 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), padding='same')(conv3a)
-conv4 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), activation='relu', padding='same')(conv3a)
+    # conv4 kernel 3x3, stride 1x1
+    # ---> 16x16x128filters
+    skip_conv4 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), padding='same')(conv3a)
+    conv4 = keras.layers.Conv2D(filters = 128, kernel_size = (3,3), strides = (1,1), activation='relu', padding='same')(conv3a)
 
-# Residual connection
-conv4r = keras.layers.add([skip_conv4, conv4])
+    # Residual connection
+    conv4r = keras.layers.add([skip_conv4, conv4])
 
-# conc kernel 1x1, stride 1x1
-# ---> 16x16x128 (same as before)
-conv5 = keras.layers.Conv2D(filters = 256, kernel_size = (3,3), strides = (1,1), activation='relu')(conv4r)
+    # conc kernel 1x1, stride 1x1
+    # ---> 16x16x128 (same as before)
+    conv5 = keras.layers.Conv2D(filters = 256, kernel_size = (3,3), strides = (1,1), activation='relu')(conv4r)
 
-# THE RESULT OF THIS LAYER IS THE STATE REPRESENTATION
-r = keras.layers.Conv2D(filters = 256, kernel_size = (1,1), strides=(1,1), activation='relu')(conv5)
+    # THE RESULT OF THIS LAYER IS THE STATE REPRESENTATION
+    r = keras.layers.Conv2D(filters = 256, kernel_size = (1,1), strides=(1,1), activation='relu')(conv5)
 
-# Define the model
-model = keras.Model(inputs=[input_vis, input_pos], outputs=r)
+    # Define the model
+    model = keras.Model(inputs=[input_vis, input_pos], outputs=r)
+    return model
 
 # check if model outputs
-inV = np.zeros((1,64,64,3))
-inP = np.zeros((1,16,16,7)) # TODO: should reshape within model itself
-print(model.predict([inV, inP], verbose=1))
-keras.utils.plot_model(model, to_file='model.png')
+# inV = np.zeros((1,64,64,3))
+inP = np.zeros((1,1,1,7))
+
+root_path = '../../Datasets'
+data_reader = DataReader(dataset='jaco', context_size=5, root=root_path)
+data = data_reader.read(batch_size=12)
+
+model = Encoder()
+# model.compile(optimizer='rmsprop',
+#               loss='categorical_crossentropy',
+#               metrics=['accuracy'])
+# model.fit([data.query.context.frames, data.query.context.cameras])  # starts training
+sess = tf.Session()
+K.set_session(sess)
+with sess.as_default():
+    print(model.predict([data.query.context.frames[0].eval(session=sess), data.query.context.cameras[0].eval(session=sess)], verbose=1))
+keras.utils.plot_model(model, to_file='representation_net.png')

GQN/generator.py
@@ -1,11 +1,8 @@
 import keras
 import numpy as np
 import tensorflow as tf
-from skipConvLSTM import SkipConvLSTM
+from keras import backend as K
 
-# Generator model takes the state representation r
-#  and the _____ vq as inputs
-#  and returns u, the ___________
 def Generator():
     # INPUTS
     vq = keras.layers.Input(shape=(16,16,256), name="vq")
@@ -14,25 +11,24 @@ def Generator():
     # INIT CONVLSTM CELL VALUES
     h0 = keras.layers.Input(tensor=tf.constant(np.zeros((1,16,16,256)), dtype='float32'), name="h0") # p26 of paper indicates this is initialized as 0
     c0 = keras.layers.Input(tensor=tf.constant(np.zeros((1,16,16,256)), dtype='float32'), name="c0")
-    u0 = keras.layers.Input(tensor=tf.constant(np.zeros((1,4,4,256)), dtype='float32'), name="u0")
-
+    u0 = keras.layers.Input(tensor=tf.constant(np.zeros((1,64,64,256)), dtype='float32'), name="u0")
 
     # TODO: How many cells in paper????
+    # TODO: LOOP?
     cell0 = GeneratorCell()([vq, r, h0, c0, u0])
-    print(cell0)
-    # cell1 = GeneratorCell()(inputs=[vq, r] + cell0)
-    # cell2 = GeneratorCell()(inputs=[vq, r] + cell1)
+    cell1 = GeneratorCell()(inputs=[vq,r]+cell0)
+    cell2 = GeneratorCell()(inputs=[vq, r] + cell1)
 
     # last conv on u (kernel 1x1, stride 1x1)
     # WHAT IS THIS OUTPUT ?
-    # something = keras.layers.Conv2D(
-    #                         filters = 256, # ???
-    #                         kernel_size = (1,1),
-    #                         strides = (1,1)
-    #                         # activation?
-    #                     )(cell0[2])
+    something = keras.layers.Conv2D(
+                            filters = 256, # ???
+                            kernel_size = (1,1),
+                            strides = (1,1)
+                            # activation?
+                        )(cell2[2])
 
-    model = keras.Model(inputs=[vq, r], outputs=cell0)
+    model = keras.Model(inputs=[vq, r, h0, c0, u0], outputs=something)
     return model
 
 
@@ -45,11 +41,9 @@ def GeneratorCell():
     r = keras.layers.Input(shape=(16,16,256), name="r")
     h0 = keras.layers.Input(shape=(16,16,256), name="h_i") # p26 of paper indicates this is initialized as 0
     c0 = keras.layers.Input(shape=(16,16,256), name="c_i")
-    u0 = keras.layers.Input(shape=(4,4,256), name="u_i")
+    u0 = keras.layers.Input(shape=(64,64,256), name="u_i")
 
-    print("hi")
     # (0) get z from h0
-    # TODO: IS IT JUST CONV'D ON THE FIRST LAYERS LIKE P38 SUGGESTS?
     z = keras.layers.Conv2D(
                     filters = 256,
                     kernel_size = (5,5),
@@ -59,7 +53,7 @@ def GeneratorCell():
                 )(h0)
 
     # (1) concatenate h0, v, r, z
-    concat = keras.layers.concatenate([h0, vq, r, z], axis=0)
+    concat = keras.layers.concatenate([h0, vq, r, z])
 
     # (2a) sig0 aka forget gate
     forget = keras.layers.Conv2D(
@@ -98,16 +92,13 @@ def GeneratorCell():
             )(concat)
 
     # (3) update context/state
-    # tmp1 = keras.layers.multiply([c0, forget])
-    # tmp2 = keras.layers.multiply([inp_gate, candidates])
-    # c = keras.layers.add([tmp1, tmp2])
     c = keras.layers.add([keras.layers.multiply([c0, forget]), keras.layers.multiply([inp_gate, candidates])])
 
     # (4) update output/h
     h = keras.layers.add([keras.layers.Activation('tanh')(c), output])
 
     # (5) u = u0 + delta?(h) (kernel 4x4, stride 4x4)
-    delta = keras.layers.Conv2D( # TODO: should be transpose
+    delta = keras.layers.Conv2DTranspose( # TODO: should be transpose
                 filters = 256,
                 kernel_size=(4,4),
                 strides=(4,4)
@@ -115,11 +106,10 @@ def GeneratorCell():
 
     u = keras.layers.add([u0, delta])
 
-    return keras.Model(inputs=[vq, r, h0, c0, u0], outputs=c)
+    return keras.Model(inputs=[vq, r, h0, c0, u0], outputs=[h, c, u])
 
 
 inp = np.zeros((1,16,16,256))
-model = Generator()([inp,inp])
-print(model)
+model = Generator()
+keras.utils.plot_model(model, to_file='generator_net.png')
 print(model.predict([inp, inp], verbose=1))
-keras.utils.plot_model(model, to_file='generator1.png')